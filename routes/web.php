a9d6f24176c5e3125203fd323e2604580d911c50
Seb Kay <seb@sebkay.com>
2022-12-07 19:45:55 +0000

    Add register routes

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,18 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
     Route::controller(LoginController::class)->group(function () {
         Route::get('login', 'show')->name('login');
         Route::post('login', 'store')->name('login.store');
+    });
+
+    Route::controller(RegisterController::class)->group(function () {
+        Route::get('register', 'show')->name('register');
+        Route::post('register', 'store')->name('register.store');
     });
 });
 
