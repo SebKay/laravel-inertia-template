c8ccefb90f24837cea981f5309bccc0794a74a65
Seb Kay <seb@sebkay.com>
2022-12-07 19:31:51 +0000

    Add Vue page component for logging in

---
+++ resources/js/Components/AppButton.vue
@@ -0,0 +1,30 @@
+<template>
+    <button :class="getCssClasses()" @click="$emit('activated', true)">
+        <span class="btn__text" v-text="text"></span>
+    </button>
+</template>
+
+<script>
+    export default {
+        emits: ["activated"],
+
+        props: {
+            text: {
+                type: String,
+                default: "Submit",
+            },
+        },
+
+        data() {
+            return {
+                cssClasses: ["btn"],
+            };
+        },
+
+        methods: {
+            getCssClasses() {
+                return this.cssClasses.join(" ");
+            },
+        },
+    };
+</script>
