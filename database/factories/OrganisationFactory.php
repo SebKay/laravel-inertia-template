b4ef2f4ec9d0c029c252541aa690704dde076dc1
Seb Kay <seb@sebkay.com>
2022-12-07 18:51:15 +0000

    Create Organisation model (with factory and seeder)

---
+++ database/factories/OrganisationFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Organisation>
+ */
+class OrganisationFactory extends Factory
+{
+    public function definition()
+    {
+        return [
+            'name'    => $this->faker->company,
+            'user_id' => User::factory(),
+        ];
+    }
+}
