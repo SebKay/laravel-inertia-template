0c706dcac76de005fbe23771ef8a0102b4b4f1a9
Seb Kay <seb@sebkay.com>
2022-12-09 16:39:55 +0000

    Fix PSR-12 issues

--- routes/web.php
+++ routes/web.php
@@ -6,44 +6,46 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
-/**
- * Register
- */
 Route::middleware('guest')->group(function () {
+    /**
+     * Register
+     */
     Route::controller(RegisterController::class)->group(function () {
         Route::get('register', 'show')->name('register');
         Route::post('register', 'store')->name('register.store');
     });
 });
 
-/**
- * Login
- */
 Route::middleware('guest')->group(function () {
+    /**
+     * Login
+     */
     Route::controller(LoginController::class)->group(function () {
         Route::get('login', 'show')->name('login');
         Route::post('login', 'store')->name('login.store');
     });
 });
 
-/**
- * Logout
- */
 Route::middleware(['auth'])->group(function () {
+    /**
+     * Logout
+     */
     Route::post('logout', [LoginController::class, 'destroy'])->name('logout');
 });
 
-/**
- * Home
- */
+
 Route::middleware('auth')->group(function () {
+    /**
+     * Home
+     */
     Route::get('/', [HomeController::class, 'index'])->name('home');
 });
 
-/**
- * Account
- */
+
 Route::middleware('auth')->group(function () {
+    /**
+     * Account
+     */
     Route::prefix('account')->group(function () {
         Route::controller(AccountController::class)->group(function () {
             Route::get('', 'edit')->name('account.edit');
