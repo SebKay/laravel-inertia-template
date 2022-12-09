5e5369fb12ed957341504c3b4dae7cdc3fd8880a
Seb Kay <seb@sebkay.com>
2022-12-09 16:45:08 +0000

    Create testing env file

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -15,6 +15,8 @@ trait CreatesApplication
     {
         $app = require __DIR__.'/../bootstrap/app.php';
 
+        $app->loadEnvironmentFrom('.env.testing');
+
         $app->make(Kernel::class)->bootstrap();
 
         return $app;
