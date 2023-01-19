2d2d483258b84f02a05cefbbb521ee58b31619e0
Seb Kay <seb@sebkay.com>
2023-01-19 20:27:18 +0000

    Tidy up Inertia request handler

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -3,22 +3,16 @@
 namespace App\Http\Middleware;
 
 use App\Http\Resources\UserResource;
-use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Inertia\Middleware;
 
 class HandleInertiaRequests extends Middleware
 {
     public function share(Request $request): array
     {
-        $isLoggedIn = Auth::check();
-        $user       = $isLoggedIn ? UserResource::make(User::find(Auth::id())) : [];
-
         return array_merge(parent::share($request), [
             'auth' => [
-                'isLoggedIn' => $isLoggedIn,
-                'user'       => $user,
+                'user' => $request->user() ? UserResource::make($request->user()) : [],
             ],
             'notice' => $request->session()->get('notice') ?: [
                 'type'    => '',
