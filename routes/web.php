49ed1c2fe54afda372c95b3b29587e1c8db8d88a
Seb Kay <seb@sebkay.com>
2023-01-19 20:43:17 +0000

    Tidy up web routes file

--- routes/web.php
+++ routes/web.php
@@ -6,50 +6,36 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('guest')->group(function () {
-    /**
-     * Register
-     */
-    Route::controller(RegisterController::class)->group(function () {
+Route::controller(RegisterController::class)
+    ->middleware(['guest'])
+    ->group(function () {
         Route::get('register', 'show')->name('register');
         Route::post('register', 'store')->name('register.store');
     });
-});
 
-Route::middleware('guest')->group(function () {
-    /**
-     * Login
-     */
-    Route::controller(LoginController::class)->group(function () {
+Route::controller(LoginController::class)
+    ->middleware(['guest'])
+    ->group(function () {
         Route::get('login', 'show')->name('login');
         Route::post('login', 'store')->name('login.store');
     });
-});
 
-Route::middleware(['auth'])->group(function () {
-    /**
-     * Logout
-     */
-    Route::post('logout', [LoginController::class, 'destroy'])->name('logout');
-});
-
-
-Route::middleware('auth')->group(function () {
-    /**
-     * Home
-     */
-    Route::get('/', [HomeController::class, 'index'])->name('home');
-});
-
-
-Route::middleware('auth')->group(function () {
-    /**
-     * Account
-     */
-    Route::prefix('account')->group(function () {
-        Route::controller(AccountController::class)->group(function () {
-            Route::get('', 'edit')->name('account.edit');
-            Route::patch('', 'update')->name('account.update');
-        });
+Route::controller(LoginController::class)
+    ->middleware(['auth'])
+    ->group(function () {
+        Route::post('logout', 'destroy')->name('logout');
     });
-});
+
+Route::controller(HomeController::class)
+    ->middleware(['auth'])
+    ->group(function () {
+        Route::get('/', 'index')->name('home');
+    });
+
+Route::controller(AccountController::class)
+    ->prefix('account')
+    ->middleware(['auth'])
+    ->group(function () {
+        Route::get('', 'edit')->name('account.edit');
+        Route::patch('', 'update')->name('account.update');
+    });
