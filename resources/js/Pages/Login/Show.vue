5a33b030877c24e8897b72f1dbb6957bc8405d6b
Seb Kay <seb@sebkay.com>
2023-02-18 22:47:11 +0000

    Tweak styling of login page

--- resources/js/Pages/Login/Show.vue
+++ resources/js/Pages/Login/Show.vue
@@ -1,7 +1,7 @@
 <template>
     <Head :title="title" />
 
-    <h1 v-text="title" class="mb-40"></h1>
+    <h1 v-text="title" class="mb-40 text-center"></h1>
 
     <form class="form" @submit.prevent="login">
         <div class="form__section">
@@ -53,6 +53,7 @@
                     <AppButton
                         text="Login"
                         tabindex="4"
+                        type="full"
                         :disabled="form.processing"
                     />
                 </div>
@@ -60,7 +61,7 @@
         </div>
     </form>
 
-    <div class="mt-30">
+    <div class="mt-30 text-center">
         <Link :href="route('register')">Register</Link>
     </div>
 </template>
