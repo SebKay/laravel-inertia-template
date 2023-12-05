6088b17cb686f249a644902ce607052ba191d1c7
Seb Kay <seb@sebkay.com>
2023-12-05 18:14:48 +0000

    Move Filament form and table definitions into models

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -4,7 +4,6 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\UserResource\Pages;
 use App\Models\User;
-use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
@@ -19,61 +18,13 @@ class UserResource extends Resource
     public static function form(Form $form): Form
     {
         return $form
-            ->schema([
-                Forms\Components\TextInput::make('first_name')
-                    ->autofocus()
-                    ->required()
-                    ->maxLength(255),
-
-                Forms\Components\TextInput::make('last_name')
-                    ->required()
-                    ->maxLength(255),
-
-                Forms\Components\TextInput::make('email')
-                    ->required()
-                    ->email()
-                    ->maxLength(255),
-
-                Forms\Components\TextInput::make('password')
-                    ->password()
-                    ->maxLength(255),
-
-                Forms\Components\DateTimePicker::make('email_verified_at'),
-            ]);
+            ->schema(User::filamentForm());
     }
 
     public static function table(Table $table): Table
     {
         return $table
-            ->columns([
-                Tables\Columns\TextColumn::make('name')
-                    ->getStateUsing(fn ($record) => $record->fullName)
-                    ->searchable()
-                    ->sortable(),
-
-                Tables\Columns\TextColumn::make('email')
-                    ->searchable()
-                    ->sortable(),
-
-                Tables\Columns\IconColumn::make('email_verified')
-                    ->getStateUsing(fn ($record) => $record->email_verified_at)
-                    ->boolean()
-                    ->sortable(),
-
-                Tables\Columns\TextColumn::make('roles')
-                    ->getStateUsing(fn ($record) => $record->roles->pluck('name')->join(', '))
-                    ->searchable()
-                    ->sortable(),
-
-                Tables\Columns\TextColumn::make('created_at')
-                    ->sortable(),
-
-                Tables\Columns\TextColumn::make('updated_at')
-                    ->sortable(),
-            ])
-            ->filters([
-                //
-            ])
+            ->columns(User::filamentTable())
             ->actions([
                 Tables\Actions\EditAction::make(),
             ])
@@ -82,13 +33,6 @@ class UserResource extends Resource
                     Tables\Actions\DeleteBulkAction::make(),
                 ]),
             ]);
-    }
-
-    public static function getRelations(): array
-    {
-        return [
-            //
-        ];
     }
 
     public static function getPages(): array
--- app/Models/User.php
+++ app/Models/User.php
@@ -5,9 +5,11 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 
 use App\Enums\Role;
+use Filament\Forms;
 use Filament\Models\Contracts\FilamentUser;
 use Filament\Models\Contracts\HasName;
 use Filament\Panel;
+use Filament\Tables;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -86,5 +88,60 @@ class User extends Authenticatable implements FilamentUser, HasName
     public function getFilamentName(): string
     {
         return $this->fullName;
+    }
+
+    public static function filamentForm()
+    {
+        return [
+            Forms\Components\TextInput::make('first_name')
+                ->autofocus()
+                ->required()
+                ->maxLength(255),
+
+            Forms\Components\TextInput::make('last_name')
+                ->required()
+                ->maxLength(255),
+
+            Forms\Components\TextInput::make('email')
+                ->required()
+                ->email()
+                ->maxLength(255),
+
+            Forms\Components\TextInput::make('password')
+                ->password()
+                ->maxLength(255),
+
+            Forms\Components\DateTimePicker::make('email_verified_at'),
+        ];
+    }
+
+    public static function filamentTable()
+    {
+        return [
+            Tables\Columns\TextColumn::make('name')
+                ->getStateUsing(fn ($record) => $record->fullName)
+                ->searchable()
+                ->sortable(),
+
+            Tables\Columns\TextColumn::make('email')
+                ->searchable()
+                ->sortable(),
+
+            Tables\Columns\IconColumn::make('email_verified')
+                ->getStateUsing(fn ($record) => $record->email_verified_at)
+                ->boolean()
+                ->sortable(),
+
+            Tables\Columns\TextColumn::make('roles')
+                ->getStateUsing(fn ($record) => $record->roles->pluck('name')->join(', '))
+                ->searchable()
+                ->sortable(),
+
+            Tables\Columns\TextColumn::make('created_at')
+                ->sortable(),
+
+            Tables\Columns\TextColumn::make('updated_at')
+                ->sortable(),
+        ];
     }
 }
