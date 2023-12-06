6492af18de6d4da42f11c78a685834e623fffd6b
Seb Kay <seb@fhoke.com>
2023-12-06 10:06:11 +0000

    Update AuthServiceProvider.php

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 
+use App\Enums\Role;
 use App\Models\User;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
@@ -27,7 +28,7 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         Gate::define('viewPulse', function (User $user) {
-            return $user->hasRole('admin');
+            return $user->hasRole(Role::SUPER_ADMIN->value);
         });
     }
 }
