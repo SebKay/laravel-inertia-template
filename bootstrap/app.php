1a6b46932de164ceda97de558facacc527f10732
Seb Kay <seb@sebkay.com>
2024-03-23 13:00:31 +0000

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
-            'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class
+            'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
         ]);
 
         $middleware->web(append: [
