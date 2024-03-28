a39a214dcdd4cedbf4354d1bf02ab01aa8b0b473
Seb Kay <seb@sebkay.com>
2024-03-28 19:09:05 +0000

    Update AuthServiceProvider.php

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -11,8 +11,10 @@ class AuthServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
+        // @codeCoverageIgnoreStart
         Gate::define('viewPulse', function (User $user) {
             return $user->hasRole(Role::SUPER_ADMIN->value);
         });
+        // @codeCoverageIgnoreEnd
     }
 }
