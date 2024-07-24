ee73c0866500d1b0508e45b9680cbd1973957ae7
Seb Kay <seb@sebkay.com>
2024-07-24 18:54:37 +0100

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use App\Models\User;
-use Carbon\Carbon;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Pulse\Facades\Pulse;
