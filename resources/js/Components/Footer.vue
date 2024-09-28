7a5b2466e283454f64b8c74ae2a74edc8a1b6ba4
Seb Kay <seb@sebkay.com>
2024-09-28 07:50:56 +0100

    Update Footer.vue

--- resources/js/Components/Footer.vue
+++ resources/js/Components/Footer.vue
@@ -2,7 +2,13 @@
     <footer class="border-t border-brand-200 px-4 sm:px-6 xl:px-8">
         <div class="mx-auto max-w-7xl py-4 xl:py-8">
             <p class="text-sm">
-                &copy; 2024 Template
+                &copy; 2024
+                <a
+                    href="https://sebkay.com/"
+                    class="underline hover:decoration-transparent transition-colors ease-in-out duration-200"
+                    target="_blank"
+                    >Seb Kay</a
+                >. All rights reserved.
             </p>
         </div>
     </footer>
