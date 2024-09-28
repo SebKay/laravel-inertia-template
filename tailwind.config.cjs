3cd7ed9e086a1eb6b3c4f546af90733427993a1d
Seb Kay <seb@sebkay.com>
2024-09-28 06:47:37 +0100

    Add "brand" color palette to Tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,9 +5,27 @@ module.exports = {
         "./resources/**/*.js",
         "./resources/**/*.vue",
     ],
+
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'brand': {
+                    '50': '#f2f9f9',
+                    '100': '#ddeff0',
+                    '200': '#bfe0e2',
+                    '300': '#92cace',
+                    '400': '#5faab1',
+                    '500': '#438e96',
+                    '600': '#3b757f',
+                    '700': '#356169',
+                    '800': '#325158',
+                    '900': '#2d464c',
+                    '950': '#1a2c32',
+                },
+            },
+        },
     },
+
     plugins: [
         require('@tailwindcss/forms'),
     ],
