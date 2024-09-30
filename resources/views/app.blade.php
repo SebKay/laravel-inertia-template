fe5d707bc71caaf8a607a89824cc6be5fdcee245
Seb Kay <seb@sebkay.com>
2024-09-30 06:35:42 +0100

    Remove unused Google fonts embed

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,10 +5,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:wght@400;600;700&display=swap" rel="stylesheet">
-
     @routes
 
     @vite(['resources/scss/app.scss', 'resources/js/app.js'])
