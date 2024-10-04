2e23140a7c847f5e7c8f4701b4b2ab2104b8337f
Seb Kay <seb@sebkay.com>
2024-10-04 06:52:14 +0100

    Remove `vite-svg-loader`

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
-import svgLoader from 'vite-svg-loader';
 
 export default defineConfig({
     plugins: [
@@ -19,12 +18,6 @@ export default defineConfig({
                     base: null,
                     includeAbsolute: false,
                 },
-            },
-        }),
-
-        svgLoader({
-            svgoConfig: {
-                multipass: true,
             },
         }),
     ],
