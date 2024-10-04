1ae5768f3338e0ec62f4fb57c845518f15d01e83
Seb Kay <seb@sebkay.com>
2024-10-04 07:11:23 +0100

    Use Vite prefetch

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\User;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Pulse\Facades\Pulse;
 
@@ -12,6 +13,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         JsonResource::withoutWrapping();
+
+        Vite::prefetch(concurrency: 3);
 
         // @codeCoverageIgnoreStart
         Pulse::users(function ($ids) {
