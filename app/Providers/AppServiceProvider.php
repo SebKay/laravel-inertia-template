f61baab2eeadd23c55f8cd7941e9029269a04c26
Seb Kay <seb@fhoke.com>
2025-03-11 07:40:48 +0000

    Prevent request exceptions from being truncated

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Models\User;
+use Illuminate\Http\Client\RequestException;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
@@ -19,6 +20,8 @@ class AppServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
+        RequestException::dontTruncate();
+
         JsonResource::withoutWrapping();
 
         Vite::prefetch(concurrency: 3);
