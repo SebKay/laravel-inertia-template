1092581614c4796cfece5cba3a3ecb726b1a378c
Seb Kay <seb@fhoke.com>
2025-03-27 07:34:37 +0000

    Add text content

--- resources/js/Pages/Elements.vue
+++ resources/js/Pages/Elements.vue
@@ -8,8 +8,8 @@
     ></h1>
 
     <div class="bg-white rounded-2xl xl:p-10 p-6 border border-brand-200">
-        <div>
-            <div class="space-y-6">
+        <div class="space-y-10 divide-y divide-brand-200">
+            <div class="space-y-6 pb-10">
                 <h4 class="heading h4">
                     Forms
                 </h4>
@@ -106,6 +106,73 @@
                         </div>
                     </div>
                 </form>
+            </div>
+
+            <div class="space-y-6">
+                <h4 class="heading h4">
+                    Content
+                </h4>
+
+                <div class="content">
+                    <h1>Heading 1</h1>
+                    <h2>Heading 2</h2>
+                    <h3>Heading 3</h3>
+                    <h4>Heading 4</h4>
+                    <h5>Heading 5</h5>
+                    <h6>Heading 6</h6>
+                    <p>
+                        Lorem ipsum dolor sit <strong>bold</strong> amet <em>italic</em> consectetur adipisicing elit.
+                        Hic praesentium consectetur eum <a href="#">text link</a> laudantium optio temporibus. Beatae,
+                        nulla natus aut accusamus sint culpa at itaque, voluptates architecto laboriosam ipsum
+                        voluptatem ducimus.
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Est ad totam distinctio consequatur
+                        quae a pariatur asperiores obcaecati error officiis perspiciatis et similique sit quas, rerum
+                        eius perferendis dolorum culpa.
+                    </p>
+                    <ul>
+                        <li>Unordered List Item 1</li>
+                        <li>Unordered List Item 2</li>
+                        <li>Unordered List Item 3</li>
+                    </ul>
+                    <ol>
+                        <li>Ordered List Item 1</li>
+                        <li>Ordered List Item 2</li>
+                        <li>Ordered List Item 3</li>
+                    </ol>
+                    <blockquote>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic praesentium consectetur eum
+                        laudantium optio temporibus. Beatae, nulla natus aut accusamus sint culpa at itaque, voluptates
+                        architecto laboriosam ipsum voluptatem ducimus.
+                    </blockquote>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Table Heading 1</th>
+                                <th>Table Heading 2</th>
+                                <th>Table Heading 3</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Table Data 1</td>
+                                <td>Table Data 2</td>
+                                <td>Table Data 3</td>
+                            </tr>
+                            <tr>
+                                <td>Table Data 1</td>
+                                <td>Table Data 2</td>
+                                <td>Table Data 3</td>
+                            </tr>
+                            <tr>
+                                <td>Table Data 1</td>
+                                <td>Table Data 2</td>
+                                <td>Table Data 3</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
