47aad1fb2e36e54377b8599d43708ff0126109f1
Seb Kay <seb@fhoke.com>
2025-03-27 07:22:41 +0000

    Add simple template for testing elements

---
+++ resources/js/Pages/Elements.vue
@@ -0,0 +1,19 @@
+<template>
+
+    <Head :title="title" />
+
+    <h1
+        v-text="title"
+        class="xl:text-4xl text-3xl font-medium text-neutral-900 xl:mb-8 mb-4"
+    ></h1>
+
+    <div class="bg-white rounded-2xl xl:p-10 p-6 border border-brand-200">
+        Elements
+    </div>
+</template>
+
+<script setup>
+    import { ref } from "vue";
+
+    const title = ref("Elements");
+</script>
