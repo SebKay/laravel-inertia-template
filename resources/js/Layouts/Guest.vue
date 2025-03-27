31255694ec6840311bdf77f29423368613911d82
Seb Kay <seb@fhoke.com>
2025-03-27 10:03:31 +0000

    Adjust guest layout

--- resources/js/Layouts/Guest.vue
+++ resources/js/Layouts/Guest.vue
@@ -4,9 +4,10 @@
         <title></title>
     </Head>
 
-    <div class="min-h-full flex flex-col">
+    <div class="min-h-full flex flex-col justify-center">
         <main class="xl:py-16 py-8 px-4 sm:px-6 xl:px-8">
             <div class="mx-auto max-w-7xl">
+                <SparklesIcon class="size-10 mx-auto text-brand-800 block mb-4 xl:mb-8" />
                 <slot />
             </div>
         </main>
@@ -16,49 +17,5 @@
 </template>
 
 <script setup>
-    import { computed, defineAsyncComponent } from "vue";
-    import { usePage } from '@inertiajs/vue3'
-
-    const page = usePage();
-
-    const menu = computed(() => {
-        if (page.props.auth.loggedIn) {
-            return [
-                {
-                    label: "Dashboard",
-                    route: route("home"),
-                    condition: true,
-                    components: ['Dashboard/Index'],
-                },
-                {
-                    label: "Account",
-                    route: route("account.edit"),
-                    condition: true,
-                    components: ['Account/Edit', 'EmailVerification/Show'],
-                },
-                {
-                    label: "Logout",
-                    route: route("logout"),
-                    method: "post",
-                    condition: true,
-                    components: [],
-                },
-            ];
-        }
-
-        return [
-            {
-                label: "Login",
-                route: route("login"),
-                condition: true,
-                components: ['Login/Show'],
-            },
-            {
-                label: "Register",
-                route: route("register"),
-                condition: true,
-                components: ['Register/Show'],
-            },
-        ];
-    });
+    import { Sparkles as SparklesIcon } from 'lucide-vue-next';
 </script>
