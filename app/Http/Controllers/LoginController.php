4954ee9290c76e54a9c59ca0bab8d95d410e0dde
Seb Kay <seb@fhoke.com>
2025-03-31 15:04:43 +0100

    Redirect user to intended page after logging in

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -34,6 +34,6 @@ class LoginController extends Controller
             return redirect()->to($request->validated('redirect'));
         }
 
-        return redirect()->route('home');
+        return redirect()->intended(\route('home'));
     }
 }
