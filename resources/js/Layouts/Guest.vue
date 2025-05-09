fcf24e6f2d99d31f1ef12c67a847377e57154572
Seb Kay <seb@sebkay.com>
2025-05-09 21:58:47 +0100

    Fix alignment of guest layout on mobile

--- resources/js/Layouts/Guest.vue
+++ resources/js/Layouts/Guest.vue
@@ -4,7 +4,7 @@
         <title></title>
     </Head>
 
-    <div class="min-h-full flex flex-col justify-center">
+    <div class="md:min-h-full flex flex-col justify-center">
         <main class="xl:py-16 py-8 px-4 sm:px-6 xl:px-8">
             <div class="mx-auto max-w-7xl">
                 <SparklesIcon class="size-10 mx-auto text-brand-800 block mb-4 xl:mb-8" />
